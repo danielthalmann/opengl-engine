fdd7b388d0cbd2d154be254c3a1c6793d7a29315
Daniel Thalmann <daniel@thalmann.li>
2018-01-05 23:21:49 +0100

    Ajout de la classe de context

---
+++ Context.cpp
@@ -0,0 +1,12 @@
+#include "Context.h"
+using namespace Cagan;
+
+Context::Context()
+{
+	//ctor
+}
+
+Context::~Context()
+{
+	//dtor
+}
---
+++ Context.h
@@ -0,0 +1,19 @@
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+namespace Cagan
+{
+	class Context
+	{
+		public:
+			Context();
+			virtual ~Context();
+
+		protected:
+
+		private:
+	};
+
+}
+
+#endif // CONTEXT_H
